31ea99a99916651342713923b6417b094da59bdc
Alexanderkabui <alexanderkabua@gmail.com>
2020-12-29 14:48:20 +0300

    refactor code for scraping

--- app.py
+++ app.py
@@ -2,9 +2,32 @@ import bs4
 from urllib.request import urlopen  as uReq
 from bs4 import BeautifulSoup as soup
 
-myUrl="https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card"
 
-myUrl='http://dl2.mojdl.com/upload/Tv-Series/Better%20Call%20Saul/S04/720p/'
+def scraper_site(my_url):
+	uClient=uReq(myUrl)
+	page=uClient.read()
+	uClient=close()
+
+def parser(page):
+	page_soup=soup(page,"html.parser")
+	cards=page_soup.findAll("div",{"class":"item-container"})
+
+def write_to_file(cards,filename):
+	f=open(filename,"w")
+	headers="brand,title,shipping\n"
+	f.write(headers)
+	for card in cards:
+	
+	brand=card.find("a",{"class":"item-brand"}).img["title"].replace(",","|")
+	title=card.find("a",{"class":"item-title"}).text.replace(",","|")
+
+	shipping=card.find("li",{"class":"price-ship"}).text.replace(",","|").strip()
+
+	f.write(brand+","+title +","+shipping+"\n")
+
+	f.close()
+
+myUrl="https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20card"
 uClient=uReq(myUrl)
 
 page=uClient.read()
